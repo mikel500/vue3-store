ed4b02f7d824363078fa92f935f96c573e4ac13c
mikelmif <mikel@kydemy.com>
2021-09-12 16:09:33 +0200

    Unnecessary code has been deleted

--- src/composable/useFetchProduct.js
+++ src/composable/useFetchProduct.js
@@ -1,34 +1,28 @@
 import { useRoute } from 'vue-router'
 import { ref, watch } from 'vue'
-import cartService from '@/services/cartService.js';
-import getLanguage from '@/services/languageService.js';
+import cartService from '@/services/cartService.js'
+import getLanguage from '@/services/languageService.js'
 
 export default () => {
   const route = useRoute()
   const itemQuantity = ref(1)
   const { language } = getLanguage()
-  const { items, addToCart } = cartService()
+  const { addToCart } = cartService()
+  const products = require(`../plants/${language.value}.json`)
   const productId = parseInt(route.params.productId)
-  const productData = ref(items.value.find(product => product.id === productId))
-
-  if (productData.value) {
-    itemQuantity.value = productData.value.quantity
-  } else {
-    const products = require(`../plants/${language.value}.json`)
-    productData.value = products.find(product => product.id === productId)
-  }
+  const productData = ref(products.find((product) => product.id === productId))
 
   watch(language, (newValue) => {
     const products = require(`../plants/${newValue}.json`)
-    productData.value = products.find(product => product.id === productId)
+    productData.value = products.find((product) => product.id === productId)
   })
-  
+
   const getOutlineStar = (n) => 5 - n
-  
+
   return {
     getOutlineStar,
     productData,
     itemQuantity,
-    addToCart
+    addToCart,
   }
 }
