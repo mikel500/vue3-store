f240aaa1d9e400ed447db41247cae74c06f51b15
mikelmif <mikel@kydemy.com>
2021-09-12 16:08:01 +0200

    Syntax updated to vue 3

--- src/components/filters/PriceFilter.vue
+++ src/components/filters/PriceFilter.vue
@@ -56,37 +56,39 @@
           @input="$emit('update:price', $event.target.value)"
         />
       </div>
-    </div>
-    <div class="flex justify-end">
-      <button @click="$emit('cleanPriceFilter', 'price')">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-          />
-        </svg>
-      </button>
+      <div class="flex justify-end">
+        <button @click="$emit('cleanPriceFilter', 'price')">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
   name: "PriceFilter",
   props: {
     price: { type: Number, required: true }
   },
-  data () {
+  setup () {
+    const showPriceFilter = ref(true)
     return {
-      showPriceFilter: true
+      showPriceFilter
     }
   }
 };
