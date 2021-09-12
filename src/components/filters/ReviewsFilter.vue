3f92e7e8c2a4cd7781151f26bb21c3dc25010928
mikelmif <mikel@kydemy.com>
2021-09-12 16:08:38 +0200

    Syntax updated to vue 3 and improved functionality

--- src/components/filters/ReviewsFilter.vue
+++ src/components/filters/ReviewsFilter.vue
@@ -42,6 +42,45 @@
         :key="'star_'+star"
       >
         <button
+          v-if="selectedOption === star"
+          class="w-full flex rounded-md text-base font-medium mt-2 mb-2 bg-gray-700"
+          @click="getFilterValue(star)"
+        >
+          <svg
+            v-for="n in star"
+            :key="n"
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            viewBox="0 0 20 20"
+            fill="#ffa651"
+          >
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+          </svg>
+          <svg
+            v-for="n in getOutlineStar(star)"
+            :key="n"
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="#ffa651"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+            />
+          </svg>
+          <span
+            v-if="star !== 5"
+            class="pl-2 text-white"
+          >
+            {{$t('store.filters.reviews_more')}}
+          </span>
+        </button>
+        <button
+          v-else
           class="w-full flex rounded-md text-base font-medium mt-2 mb-2"
           @click="getFilterValue(star)"
         >
@@ -71,48 +110,74 @@
               d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
             />
           </svg>
-             &nbsp;&nbsp;{{$t('store.filters.reviews_more')}}
+          <span
+            v-if="star !== 5"
+            class="pl-2"
+          >
+            {{$t('store.filters.reviews_more')}}
+          </span>
+        </button>
+      </li>
+      <li class="flex justify-end">
+        <button @click="cleanReviewsFilter()">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+            />
+          </svg>
         </button>
       </li>
     </ol>
-    <div class="flex justify-end">
-      <button @click="$emit('cleanReviewsFilter', 'reviews')">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-          />
-        </svg>
-      </button>
-    </div>
   </div>
 </template>
 
 <script>
+import { ref, watch } from 'vue'
+import useProductFilters from '@/composable/useProductFilters'
 export default {
   name: "ReviewsFilter",
-  data () {
+  setup (_, context) {
+
+    const showReviewFilter = ref(true)
+    const selectedOption = ref(null)
+    const { stars } = useProductFilters()
+
+    const reverseKeys = (n) => Array.from(Array(n).keys()).reverse()
+
+    const getOutlineStar = (n) => 5 - n
+
+    const getFilterValue = (newVal) => {
+      selectedOption.value = newVal
+      context.emit("reviewsFilterUpdated", { value: newVal, filter: 'stars' })
+    }
+
+    const cleanReviewsFilter = () => {
+      selectedOption.value = null
+      context.emit('cleanReviewsFilter', 'reviews')
+    }
+
+    watch(stars, (newValue) => {
+      if (newValue === null) {
+        selectedOption.value = null
+      }
+    })
+
     return {
-      showReviewFilter: true,
-    };
-  },
-  methods: {
-    reverseKeys (n) {
-      return Array.from(Array(n).keys()).reverse()
-    },
-    getOutlineStar (n) {
-      return 5 - n
-    },
-    getFilterValue (newVal) {
-      this.$emit("reviewsFilterUpdated", { value: newVal, filter: 'stars' });
+      showReviewFilter,
+      selectedOption,
+      reverseKeys,
+      getOutlineStar,
+      getFilterValue,
+      cleanReviewsFilter,
     }
   }
 };
